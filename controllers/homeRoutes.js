e86b756deac1767d501c20d4ad74af1205463e64
Ivana Djordjevic <127266659+runningaroundintheabyss@users.noreply.github.com>
2023-12-22 15:55:46 -0800

    removed the profile option because it was not a part of the criteria

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -101,29 +101,9 @@ router.get('/edit/:id', async (req, res) => {
   }
 });
 
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
-    });
-
-    const user = userData.get({ plain: true });
-    console.log(user);
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    console.log(err)
-    res.status(500).json(err);
-  }
-});
-
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
 
